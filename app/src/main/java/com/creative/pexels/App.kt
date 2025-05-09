68a83ab0d430cee50ee192dd1501a5ed1b16e9ec
dantech0xff <danhtran.dev@outlook.com>
2025-05-09 23:26:44 +0700

    Init Project

---
+++ app/src/main/java/com/creative/pexels/App.kt
@@ -0,0 +1,14 @@
+package com.creative.pexels
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by dan on 9/5/25
+ *
+ * Copyright © 2025 1010 Creative. All rights reserved.
+ */
+
+@HiltAndroidApp
+class App : Application() {
+}
---
+++ app/src/main/java/com/creative/pexels/ui/launcher/LauncherActivity.kt
@@ -0,0 +1,49 @@
+package com.creative.pexels.ui.launcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.creative.pexels.ui.theme.SimplePexelsTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimplePexelsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimplePexelsTheme {
+        Greeting("Android")
+    }
+}
