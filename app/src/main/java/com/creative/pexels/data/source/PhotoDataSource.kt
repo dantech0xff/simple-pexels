3ad9795f597153f683111d84d5f8a149442d8f50
dantech0xff <danhtran.dev@outlook.com>
2025-05-10 01:48:55 +0700

    Add PhotoDataSource.kt PhotoDataSourceImpl.kt

---
+++ app/src/main/java/com/creative/pexels/data/source/PhotoDataSource.kt
@@ -0,0 +1,19 @@
+package com.creative.pexels.data.source
+
+import com.creative.pexels.data.model.Photo
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by dan on 10/5/25
+ *
+ * Copyright © 2025 1010 Creative. All rights reserved.
+ */
+
+interface PhotoDataSource {
+
+    val photoFlow: Flow<List<Photo>>
+
+    suspend fun loadPhotos(
+        query: String
+    ): Result<Int>
+}
---
+++ app/src/main/java/com/creative/pexels/data/source/PhotoDataSourceImpl.kt
@@ -0,0 +1,53 @@
+package com.creative.pexels.data.source
+
+import com.creative.pexels.data.adapter.toPhoto
+import com.creative.pexels.data.model.Photo
+import com.creative.pexels.dispatchers.AppDispatchers
+import com.creative.pexels.network.PexelsApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by dan on 10/5/25
+ *
+ * Copyright © 2025 1010 Creative. All rights reserved.
+ */
+
+@Singleton
+class PhotoDataSourceImpl @Inject constructor(
+    private val pexelsApiService: PexelsApiService,
+    private val appDispatchers: AppDispatchers
+) : PhotoDataSource {
+    companion object {
+        private const val DEFAULT_PAGE_SIZE: Int = 20
+    }
+
+    private var currentPage: Int = 1
+    private val mutex: Mutex = Mutex()
+    private val mutablePhotoFlow: MutableStateFlow<List<Photo>> = MutableStateFlow(emptyList())
+
+    override val photoFlow: Flow<List<Photo>>
+        get() = mutablePhotoFlow.asStateFlow()
+
+    override suspend fun loadPhotos(query: String) = withContext(appDispatchers.io) {
+        mutex.withLock {
+            runCatching {
+                pexelsApiService.searchPhotos(query, currentPage, perPage = DEFAULT_PAGE_SIZE).photos.map {
+                    it.toPhoto()
+                }
+            }.fold({
+                currentPage += 1
+                mutablePhotoFlow.value += it
+                Result.success(it.size)
+            }, {
+                Result.failure(it)
+            })
+        }
+    }
+}
